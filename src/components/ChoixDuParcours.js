54ff494b48c82edcd87e6c3048415c1bb2320380
ludovic Vachon <vachon.ludovic@gmail.com>
2022-07-07 10:08:09 +0200

    choixparcours

--- src/components/ChoixDuParcours.js
+++ src/components/ChoixDuParcours.js
@@ -4,20 +4,39 @@ import {FlatList, Text, TextInput, View} from 'react-native';
 import Modal from 'react-native-modal';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {matchSorter} from 'match-sorter';
 
 const ChoixDuParcours = ({parcoursChoisi}) => {
   const [isModalVisible, setModalVisible] = useState(false);
+  const [villeDejaSaisie, setVilleDejaSaisie] = useState('');
+  const [ajoutParcoursVisible, setAjoutParcoursVisible] = useState(false);
   const [listeDesParcours, setListeDesParcours] = useState(null);
   const list = [
     {id: '1', title: 'Sossay - Orches - Sérigny - saint Christophe'},
     {id: '2', title: 'Parcours 2'},
     {id: '3', title: 'Parcours 3'},
   ];
+  const listVille = [
+    {id: '11', title: 'Sossay'},
+    {id: '21', title: 'Orches'},
+    {id: '31', title: 'Sérigny'},
+    {id: '41', title: 'Saint Christophe'},
+  ];
 
   useEffect(() => {
     retrieveData();
   }, []);
-
+  console.log('villeDejaSaisie');
+  console.log(villeDejaSaisie);
+  const handleText = (e) => {
+    const resultatDeRecherche = matchSorter(
+      listVille.map((element) => element.title),
+      e,
+    );
+    console.log('resultatDeRecherche');
+    console.log(resultatDeRecherche);
+    setVilleDejaSaisie(resultatDeRecherche);
+  };
   const storeData = async (dataToStore) => {
     try {
       await AsyncStorage.setItem('listeParcours', JSON.stringify(dataToStore));
@@ -60,6 +79,24 @@ const ChoixDuParcours = ({parcoursChoisi}) => {
       </Text>
     </Pressable>
   );
+  const renderItemVille = ({item}) => (
+    <Pressable
+      onPress={() => {
+        // parcoursChoisi(item);
+        // setModalVisible(false);
+      }}>
+      <Text
+        style={{
+          color: 'white',
+          backgroundColor: 'black',
+          textAlign: 'center',
+          marginVertical: 10,
+          paddingVertical: 10,
+        }}>
+        {item.title}
+      </Text>
+    </Pressable>
+  );
   return (
     <View>
       <Pressable onPress={() => setModalVisible(true)}>
@@ -67,12 +104,10 @@ const ChoixDuParcours = ({parcoursChoisi}) => {
           ChoixDuParcours
         </Text>
       </Pressable>
-      <Modal animationType="fade" transparent={true} isVisible={isModalVisible}>
-        <FlatList
-          data={list}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        isVisible={ajoutParcoursVisible}>
         <View
           style={{
             color: 'white',
@@ -81,8 +116,50 @@ const ChoixDuParcours = ({parcoursChoisi}) => {
             marginVertical: 10,
             paddingVertical: 10,
           }}>
-          <TextInput placeholder="Saisissez ici votre nouveau parcours" />
+          <FlatList
+            data={villeDejaSaisie}
+            renderItem={renderItemVille}
+            keyExtractor={(i) => i.id}
+          />
+          <TextInput
+            placeholder="Saisissez ici votre nouveau parcours"
+            onChangeText={handleText}
+          />
         </View>
+      </Modal>
+      <Modal animationType="fade" transparent={true} isVisible={isModalVisible}>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+        <Pressable
+          onPress={() => setAjoutParcoursVisible(!ajoutParcoursVisible)}
+          style={{
+            color: 'white',
+            backgroundColor: 'lightgrey',
+            textAlign: 'center',
+            marginVertical: 10,
+            width: 70,
+            height: 70,
+            paddingVertical: 10,
+            left: 300,
+            borderRadius: 50,
+          }}>
+          <Text style={{textAlign: 'center', lineHeight: 55, fontSize: 50}}>
+            +
+          </Text>
+        </Pressable>
+        {/* {ajoutParcoursVisible && (
+          <View
+            style={{
+              color: 'white',
+              backgroundColor: 'lightgrey',
+              textAlign: 'center',
+              marginVertical: 10,
+              paddingVertical: 10,
+            }}></View>
+        )} */}
       </Modal>
     </View>
   );
